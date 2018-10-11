432baa9bcc7be094906d34ae2ed1d8999610d161
Alex Ramirez <armzprz@gmail.com>
2018-10-11 12:02:43 -0500

    Exit method

--- src/main/java/com/rocketgit/MainController.java
+++ src/main/java/com/rocketgit/MainController.java
@@ -1,4 +1,11 @@
 package com.rocketgit;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+
 public class MainController {
+
+    public void exit(ActionEvent actionEvent) {
+        Platform.exit();
+    }
 }
